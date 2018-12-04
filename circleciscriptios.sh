9b96724d124b5182ae6936fd6e1ceb2ea5639fc7
Roshan Kumar <roskumr@amazon.com>
2018-12-04 10:38:44 -0800

    Add the circleCI config file and the script to run sample app integration test

---
+++ circleciscriptios.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/awslabs/aws-sdk-ios-samples.git ios-sample-2
+cfolder=$(pwd)
+cd ios-sample-2/
+git checkout deviceFarm
+cd S3TransferUtility-Sample/Swift/
+cp $cfolder/awsconfiguration.json S3BackgroundTransferSampleSwift/awsconfiguration.json
+pod install --repo-update
+xcodebuild -workspace S3TransferUtilitySampleSwift.xcworkspace -scheme "S3TransferUtilitySampleSwiftUITests" -destination 'platform=iOS Simulator,name=iPhone 8 Plus,OS=11.3' test
+cd $cfolder
+
