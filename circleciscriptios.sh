b5ae667d6c509369811ce3473a691fbf1d9984dc
Roshan Kumar <roskumr@amazon.com>
2018-12-04 11:36:49 -0800

    Update the script

--- circleciscriptios.sh
+++ circleciscriptios.sh
@@ -5,6 +5,6 @@ git checkout deviceFarm
 cd S3TransferUtility-Sample/Swift/
 cp $cfolder/awsconfiguration.json S3BackgroundTransferSampleSwift/awsconfiguration.json
 pod install --repo-update
-xcodebuild -workspace S3TransferUtilitySampleSwift.xcworkspace -scheme "S3TransferUtilitySampleSwiftUITests" -destination 'platform=iOS Simulator,name=iPhone 8 Plus,OS=11.3' test
+xcodebuild -workspace S3TransferUtilitySampleSwift.xcworkspace -scheme "S3TransferUtilitySampleSwiftUITests" -destination 'platform=iOS Simulator,name=iPhone 8 Plus,OS=11.2' test
 cd $cfolder
 
